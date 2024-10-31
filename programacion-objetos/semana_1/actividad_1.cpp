352f79039d6a43dc6e567c3df196037b02977e45
santiago <a01255853@tec.mx>
2024-10-31 08:08:04 -0700

    actividad 1 IMC

---
+++ programacion-objetos/semana_1/actividad_1.cpp
@@ -0,0 +1,23 @@
+//  Programa para calcular IMC de usuario
+//  Santiago Cacho Herrera A01255853
+// 30/10/24
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float peso, altura;
+
+    cout << "Ingrese peso en kg: ";
+    cin >> peso;
+
+    cout << "Ingrese altura en metros: ";
+    cin >> altura;
+
+    float imc = peso / (altura * altura);
+
+    cout << "Tu IMC es: " << imc << endl;
+
+    return 0;
+}
